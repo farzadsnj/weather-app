91c0ccb76bf91ec30883fd64b853410871865402
farzadsnj <farzadsanjarani3@gmail.com>
2023-01-08 23:47:06 +0330

    co1

---
+++ src/components/search/Api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f3e0d9dbc5msh711637e9e9eac90p193263jsnffbee60f5e7e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
